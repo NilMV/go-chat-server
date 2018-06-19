b9860f2dab824501fadd7c890197d1e3f2d884ba
Nil Medvedev <nilmv@Nils-MacBook-Pro.local>
2018-06-19 23:35:40 +0300

    add gitigonre and error-handling

--- server/chat-server.go
+++ server/chat-server.go
@@ -5,17 +5,27 @@ import "fmt"
 import "bufio"
 import "strings" // only needed below for sample processing
 
+func serveClient(conn net.Conn) {
+
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	newmessage := strings.ToUpper(message)
+	conn.Write([]byte(newmessage + "\n"))
+
+}
+
 func main() {
 
 	fmt.Println("Launching server...")
-	ln, _ := net.Listen("tcp", ":8081")
-
-	conn, _ := ln.Accept()
-
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Errorf("Launching server error:", err)
+	}
 	for {
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message Received:", string(message))
-		newmessage := strings.ToUpper(message)
-		conn.Write([]byte(newmessage + "\n"))
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Errorf("Error handling connection!", err)
+		} else {
+			go serveClient(conn)
+		}
 	}
 }
